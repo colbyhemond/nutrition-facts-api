76b22b482565d56371f296fcf1e5c0c9da16cea5
Colby Hemond <hemond.colby@gmail.com>
2021-09-08 12:31:40 -0400

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const MongoClient  = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser');
+var cors = require('cors')
 
 const app = express();
 
 const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }), cors());
 
 require('./app/routes')(app, {});
 app.listen(process.env.PORT || port, () => {  console.log('We are live on ' + port);});
